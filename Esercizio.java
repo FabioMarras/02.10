92b59323f63201e29ff50ebda6ad932c882e5e83
FabioMarras <fabio.marras@yahoo.com>
2023-10-02 14:15:32 +0200

    Initial commit

---
+++ Esercizio.java
@@ -0,0 +1,12 @@
+public class Esercizio {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+   int primoNum = 2;
+   int secondoNum = 3;
+int risultato = primoNum * secondoNum;
+    System.out.println("La somma è = " + risultato);
+
+    String str = "Sono il primo numero";
+        System.out.println(str + " " +primoNum);
+    }
+}
